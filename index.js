5e979d5d722031b6bebcb2b2bc53360ae402526c
BobbyNooby <35001503+BobbyNooby@users.noreply.github.com>
2023-05-10 14:49:20 +0800

    Barebones Update

--- index.js
+++ index.js
@@ -41,16 +41,55 @@ function calculate() {
     var pantsAttackSize = parseInt(pants.options[pants.selectedIndex].getAttribute("data-attacksize"));
     var pantsIntensity = parseInt(pants.options[pants.selectedIndex].getAttribute("data-intensity"));
 
+    var enchant1 = document.getElementById("enchant1");
+    var enchant1Power = parseInt(enchant1.options[enchant1.selectedIndex].getAttribute("data-power"));
+    var enchant1Defense = parseInt(enchant1.options[enchant1.selectedIndex].getAttribute("data-defense"));
+    var enchant1Agility = parseInt(enchant1.options[enchant1.selectedIndex].getAttribute("data-agility"));
+    var enchant1AttackSpeed = parseInt(enchant1.options[enchant1.selectedIndex].getAttribute("data-attackspeed"));
+    var enchant1AttackSize = parseInt(enchant1.options[enchant1.selectedIndex].getAttribute("data-attacksize"));
+    var enchant1Intensity = parseInt(enchant1.options[enchant1.selectedIndex].getAttribute("data-intensity"));
+
+    var enchant2 = document.getElementById("enchant2");
+    var enchant2Power = parseInt(enchant2.options[enchant2.selectedIndex].getAttribute("data-power"));
+    var enchant2Defense = parseInt(enchant2.options[enchant2.selectedIndex].getAttribute("data-defense"));
+    var enchant2Agility = parseInt(enchant2.options[enchant2.selectedIndex].getAttribute("data-agility"));
+    var enchant2AttackSpeed = parseInt(enchant2.options[enchant2.selectedIndex].getAttribute("data-attackspeed"));
+    var enchant2AttackSize = parseInt(enchant2.options[enchant2.selectedIndex].getAttribute("data-attacksize"));
+    var enchant2Intensity = parseInt(enchant2.options[enchant2.selectedIndex].getAttribute("data-intensity"));
+
+    var enchant3 = document.getElementById("enchant3");
+    var enchant3Power = parseInt(enchant3.options[enchant3.selectedIndex].getAttribute("data-power"));
+    var enchant3Defense = parseInt(enchant3.options[enchant3.selectedIndex].getAttribute("data-defense"));
+    var enchant3Agility = parseInt(enchant3.options[enchant3.selectedIndex].getAttribute("data-agility"));
+    var enchant3AttackSpeed = parseInt(enchant3.options[enchant3.selectedIndex].getAttribute("data-attackspeed"));
+    var enchant3AttackSize = parseInt(enchant3.options[enchant3.selectedIndex].getAttribute("data-attacksize"));
+    var enchant3Intensity = parseInt(enchant3.options[enchant3.selectedIndex].getAttribute("data-intensity"));
+
+    var enchant4 = document.getElementById("enchant4");
+    var enchant4Power = parseInt(enchant4.options[enchant4.selectedIndex].getAttribute("data-power"));
+    var enchant4Defense = parseInt(enchant4.options[enchant4.selectedIndex].getAttribute("data-defense"));
+    var enchant4Agility = parseInt(enchant4.options[enchant4.selectedIndex].getAttribute("data-agility"));
+    var enchant4AttackSpeed = parseInt(enchant4.options[enchant4.selectedIndex].getAttribute("data-attackspeed"));
+    var enchant4AttackSize = parseInt(enchant4.options[enchant4.selectedIndex].getAttribute("data-attacksize"));
+    var enchant4Intensity = parseInt(enchant4.options[enchant4.selectedIndex].getAttribute("data-intensity"));
+
+    var enchant5 = document.getElementById("enchant5");
+    var enchant5Power = parseInt(enchant5.options[enchant5.selectedIndex].getAttribute("data-power"));
+    var enchant5Defense = parseInt(enchant5.options[enchant5.selectedIndex].getAttribute("data-defense"));
+    var enchant5Agility = parseInt(enchant5.options[enchant5.selectedIndex].getAttribute("data-agility"));
+    var enchant5AttackSpeed = parseInt(enchant5.options[enchant5.selectedIndex].getAttribute("data-attackspeed"));
+    var enchant5AttackSize = parseInt(enchant5.options[enchant5.selectedIndex].getAttribute("data-attacksize"));
+    var enchant5Intensity = parseInt(enchant5.options[enchant5.selectedIndex].getAttribute("data-intensity"));
 
     
   
     // Add the power and defense values together
-    var totalPower = accessory1Power + accessory2Power + accessory3Power + chestpiecePower + pantsPower;
-    var totalDefense = accessory1Defense + accessory2Defense + accessory3Defense + chestpieceDefense + pantsDefense;
-    var totalAgility = accessory1Agility + accessory2Agility + accessory3Agility + chestpieceAgility + pantsAgility;
-    var totalAttackSpeed = accessory1AttackSpeed + accessory2AttackSpeed + accessory3AttackSpeed + chestpieceAttackSpeed + pantsAttackSpeed;
-    var totalAttackSize = accessory1AttackSize + accessory2AttackSize + accessory3AttackSize + chestpieceAttackSize + pantsAttackSize;
-    var totalIntensity = accessory1Intensity + accessory2Intensity + accessory3Intensity + chestpieceIntensity + pantsIntensity;
+    var totalPower = accessory1Power + accessory2Power + accessory3Power + chestpiecePower + pantsPower + enchant1Power + enchant2Power + enchant3Power + enchant4Power + enchant5Power;
+    var totalDefense = accessory1Defense + accessory2Defense + accessory3Defense + chestpieceDefense + pantsDefense + enchant1Defense + enchant2Defense + enchant3Defense + enchant4Defense + enchant5Defense;
+    var totalAgility = accessory1Agility + accessory2Agility + accessory3Agility + chestpieceAgility + pantsAgility + enchant1Agility + enchant2Agility + enchant3Agility + enchant4Agility + enchant5Agility;
+    var totalAttackSpeed = accessory1AttackSpeed + accessory2AttackSpeed + accessory3AttackSpeed + chestpieceAttackSpeed + pantsAttackSpeed + enchant1AttackSpeed + enchant2AttackSpeed + enchant3AttackSpeed + enchant4AttackSpeed + enchant5AttackSpeed;
+    var totalAttackSize = accessory1AttackSize + accessory2AttackSize + accessory3AttackSize + chestpieceAttackSize + pantsAttackSize + enchant1AttackSize + enchant2AttackSize + enchant3AttackSize + enchant4AttackSize + enchant5AttackSize;
+    var totalIntensity = accessory1Intensity + accessory2Intensity + accessory3Intensity + chestpieceIntensity + pantsIntensity + enchant1Intensity + enchant2Intensity + enchant3Intensity + enchant4Intensity + enchant5Intensity;
   
     // Update the values on the page
     document.getElementById("power").textContent = totalPower;
