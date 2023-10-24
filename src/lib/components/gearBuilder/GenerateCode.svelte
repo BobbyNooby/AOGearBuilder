b03ed9176a1dc03c9bc49dee4ac0ccd322670392
woodyloody <81761800+woodyloody@users.noreply.github.com>
2023-10-24 11:45:20 +0100

    Generate and Load Patch
    Moved loadCode and generateCode functions to statsStore for global use.
    Changed copy to clipboard to non deprecated method.

--- src/lib/components/gearBuilder/GenerateCode.svelte
+++ src/lib/components/gearBuilder/GenerateCode.svelte
@@ -1,106 +1,13 @@
 <script>
-	import {
-		accessory1,
-		accessory1Gem1,
-		accessory1Gem2,
-		accessory1Gem3,
-		accessory1Enchant,
-		accessory1Modifier,
-		accessory2,
-		accessory2Gem1,
-		accessory2Gem2,
-		accessory2Gem3,
-		accessory2Enchant,
-		accessory2Modifier,
-		accessory3,
-		accessory3Gem1,
-		accessory3Gem2,
-		accessory3Gem3,
-		accessory3Enchant,
-		accessory3Modifier,
-		chestplate1,
-		chestplate1Gem1,
-		chestplate1Gem2,
-		chestplate1Gem3,
-		chestplate1Enchant,
-		chestplate1Modifier,
-		pants1,
-		pants1Gem1,
-		pants1Gem2,
-		pants1Gem3,
-		pants1Enchant,
-		pants1Modifier
-	} from '$lib/utils/statsStore';
+	import { generateCode } from '$lib/utils/statsStore';
 	import { playCorrect } from '$lib/utils/sound';
 
-	function generateCode() {
-		let code =
-			$accessory1.id +
-			'.' +
-			$accessory1Gem1.id +
-			'.' +
-			$accessory1Gem2.id +
-			'.' +
-			$accessory1Gem3.id +
-			'.' +
-			$accessory1Enchant.id +
-			'.' +
-			$accessory1Modifier.id +
-			"'" +
-			$accessory2.id +
-			'.' +
-			$accessory2Gem1.id +
-			'.' +
-			$accessory2Gem2.id +
-			'.' +
-			$accessory2Gem3.id +
-			'.' +
-			$accessory2Enchant.id +
-			'.' +
-			$accessory2Modifier.id +
-			"'" +
-			$accessory3.id +
-			'.' +
-			$accessory3Gem1.id +
-			'.' +
-			$accessory3Gem2.id +
-			'.' +
-			$accessory3Gem3.id +
-			'.' +
-			$accessory3Enchant.id +
-			'.' +
-			$accessory3Modifier.id +
-			"'" +
-			$chestplate1.id +
-			'.' +
-			$chestplate1Gem1.id +
-			'.' +
-			$chestplate1Gem2.id +
-			'.' +
-			$chestplate1Gem3.id +
-			'.' +
-			$chestplate1Enchant.id +
-			'.' +
-			$chestplate1Modifier.id +
-			"'" +
-			$pants1.id +
-			'.' +
-			$pants1Gem1.id +
-			'.' +
-			$pants1Gem2.id +
-			'.' +
-			$pants1Gem3.id +
-			'.' +
-			$pants1Enchant.id +
-			'.' +
-			$pants1Modifier.id;
+	async function generate() {
+		// generate code
+		let code = generateCode();
 
-		const clipBoard = document.createElement('textarea'); // Use 'textarea' instead of 'clipBoard'
-		clipBoard.value = code;
-		document.body.appendChild(clipBoard);
-		clipBoard.select();
-		document.execCommand('copy');
-		document.body.removeChild(clipBoard);
+		// copy text to clipboard
+		await navigator.clipboard.writeText(code);
 		playCorrect();
 	}
 </script>
@@ -109,7 +16,7 @@
 	<button
 		class=" bg-black border border-white text-white font-bold text-lg py-2 px-4 w-44"
 		style="font-family: Merriweather;"
-		on:click={generateCode}
+		on:click={generate}
 	>
 		Generate Build
 	</button>
--- src/lib/components/gearBuilder/LoadCode.svelte
+++ src/lib/components/gearBuilder/LoadCode.svelte
@@ -1,100 +1,18 @@
 <script>
-	import {
-		accessory1,
-		accessory1Gem1,
-		accessory1Gem2,
-		accessory1Gem3,
-		accessory1Enchant,
-		accessory1Modifier,
-		accessory2,
-		accessory2Gem1,
-		accessory2Gem2,
-		accessory2Gem3,
-		accessory2Enchant,
-		accessory2Modifier,
-		accessory3,
-		accessory3Gem1,
-		accessory3Gem2,
-		accessory3Gem3,
-		accessory3Enchant,
-		accessory3Modifier,
-		chestplate1,
-		chestplate1Gem1,
-		chestplate1Gem2,
-		chestplate1Gem3,
-		chestplate1Enchant,
-		chestplate1Modifier,
-		pants1,
-		pants1Gem1,
-		pants1Gem2,
-		pants1Gem3,
-		pants1Enchant,
-		pants1Modifier
-	} from '$lib/utils/statsStore';
-
-	import {
-		getAccessoryById,
-		getChestplateById,
-		getPantsById,
-		getGemById,
-		getEnchantById,
-		getModifierById
-	} from '$lib/utils/getItemById';
-
 	import { playCorrect, playWrong } from '$lib/utils/sound';
+	import { loadCode } from '$lib/utils/statsStore';
 
 	let inputString = '';
 
-	async function loadCode() {
-		try {
-			// Read text from the clipboard
-			inputString = await navigator.clipboard.readText();
-
-			// Parse the input string
-			const rows = inputString.split("'");
-			const accessory1Row = rows[0].split('.');
-			const accessory2Row = rows[1].split('.');
-			const accessory3Row = rows[2].split('.');
-			const chestplate1Row = rows[3].split('.');
-			const pants1Row = rows[4].split('.');
-
-			$accessory1 = getAccessoryById(parseInt(accessory1Row[0]));
-			$accessory1Gem1 = getGemById(parseInt(accessory1Row[1]));
-			$accessory1Gem2 = getGemById(parseInt(accessory1Row[2]));
-			$accessory1Gem3 = getGemById(parseInt(accessory1Row[3]));
-			$accessory1Enchant = getEnchantById(parseInt(accessory1Row[4]));
-			$accessory1Modifier = getModifierById(parseInt(accessory1Row[5]));
-
-			$accessory2 = getAccessoryById(parseInt(accessory2Row[0]));
-			$accessory2Gem1 = getGemById(parseInt(accessory2Row[1]));
-			$accessory2Gem2 = getGemById(parseInt(accessory2Row[2]));
-			$accessory2Gem3 = getGemById(parseInt(accessory2Row[3]));
-			$accessory2Enchant = getEnchantById(parseInt(accessory2Row[4]));
-			$accessory2Modifier = getModifierById(parseInt(accessory2Row[5]));
-
-			$accessory3 = getAccessoryById(parseInt(accessory3Row[0]));
-			$accessory3Gem1 = getGemById(parseInt(accessory3Row[1]));
-			$accessory3Gem2 = getGemById(parseInt(accessory3Row[2]));
-			$accessory3Gem3 = getGemById(parseInt(accessory3Row[3]));
-			$accessory3Enchant = getEnchantById(parseInt(accessory3Row[4]));
-			$accessory3Modifier = getModifierById(parseInt(accessory3Row[5]));
-
-			$chestplate1 = getChestplateById(parseInt(chestplate1Row[0]));
-			$chestplate1Gem1 = getGemById(parseInt(chestplate1Row[1]));
-			$chestplate1Gem2 = getGemById(parseInt(chestplate1Row[2]));
-			$chestplate1Gem3 = getGemById(parseInt(chestplate1Row[3]));
-			$chestplate1Enchant = getEnchantById(parseInt(chestplate1Row[4]));
-			$chestplate1Modifier = getModifierById(parseInt(chestplate1Row[5]));
-
-			$pants1 = getPantsById(parseInt(pants1Row[0]));
-			$pants1Gem1 = getGemById(parseInt(pants1Row[1]));
-			$pants1Gem2 = getGemById(parseInt(pants1Row[2]));
-			$pants1Gem3 = getGemById(parseInt(pants1Row[3]));
-			$pants1Enchant = getEnchantById(parseInt(pants1Row[4]));
-			$pants1Modifier = getModifierById(parseInt(pants1Row[5]));
-
+	async function load() {
+		// read string from clipboard
+		inputString = await navigator.clipboard.readText();
+		// load build into store
+		let success = loadCode(inputString);
+		// play sounds for success or error
+		if (success){
 			playCorrect();
-		} catch (error) {
+		} else {
 			playWrong();
 		}
 	}
@@ -104,7 +22,7 @@
 	<button
 		class="bg-black border border-white text-white font-bold text-lg py-2 px-4 w-44"
 		style="font-family: Merriweather;"
-		on:click={loadCode}
+		on:click={load}
 	>
 		Load Build
 	</button>
