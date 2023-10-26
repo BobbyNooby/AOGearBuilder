1e88308602b4aa667a0d899d1cff06463e3ba66d
BobbyNooby <theengineering.commandgamer@gmail.com>
2023-10-26 17:07:21 +0100

    Fixed atlantean bug AGAIN

--- src/lib/components/gearBuilder/LoadCode.svelte
+++ src/lib/components/gearBuilder/LoadCode.svelte
@@ -51,49 +51,53 @@
 			inputString = await navigator.clipboard.readText();
 
 			// Parse the input string
-			const rows = inputString.split("'");
-			const accessory1Row = rows[0].split('.');
-			const accessory2Row = rows[1].split('.');
-			const accessory3Row = rows[2].split('.');
-			const chestplate1Row = rows[3].split('.');
-			const pants1Row = rows[4].split('.');
+			if (inputString.length >= 59) {
+				//Minimum length of the code generated is 59 so this is to test if the code is valid.
+				const rows = inputString.split("'");
+				const accessory1Row = rows[0].split('.');
+				const accessory2Row = rows[1].split('.');
+				const accessory3Row = rows[2].split('.');
+				const chestplate1Row = rows[3].split('.');
+				const pants1Row = rows[4].split('.');
 
-			$accessory1 = getAccessoryById(parseInt(accessory1Row[0]));
-			$accessory1Gem1 = getGemById(parseInt(accessory1Row[1]));
-			$accessory1Gem2 = getGemById(parseInt(accessory1Row[2]));
-			$accessory1Gem3 = getGemById(parseInt(accessory1Row[3]));
-			$accessory1Enchant = getEnchantById(parseInt(accessory1Row[4]));
-			$accessory1Modifier = getModifierById(parseInt(accessory1Row[5]));
+				$accessory1 = getAccessoryById(parseInt(accessory1Row[0]));
+				$accessory1Gem1 = getGemById(parseInt(accessory1Row[1]));
+				$accessory1Gem2 = getGemById(parseInt(accessory1Row[2]));
+				$accessory1Gem3 = getGemById(parseInt(accessory1Row[3]));
+				$accessory1Enchant = getEnchantById(parseInt(accessory1Row[4]));
+				$accessory1Modifier = getModifierById(parseInt(accessory1Row[5]));
 
-			$accessory2 = getAccessoryById(parseInt(accessory2Row[0]));
-			$accessory2Gem1 = getGemById(parseInt(accessory2Row[1]));
-			$accessory2Gem2 = getGemById(parseInt(accessory2Row[2]));
-			$accessory2Gem3 = getGemById(parseInt(accessory2Row[3]));
-			$accessory2Enchant = getEnchantById(parseInt(accessory2Row[4]));
-			$accessory2Modifier = getModifierById(parseInt(accessory2Row[5]));
+				$accessory2 = getAccessoryById(parseInt(accessory2Row[0]));
+				$accessory2Gem1 = getGemById(parseInt(accessory2Row[1]));
+				$accessory2Gem2 = getGemById(parseInt(accessory2Row[2]));
+				$accessory2Gem3 = getGemById(parseInt(accessory2Row[3]));
+				$accessory2Enchant = getEnchantById(parseInt(accessory2Row[4]));
+				$accessory2Modifier = getModifierById(parseInt(accessory2Row[5]));
 
-			$accessory3 = getAccessoryById(parseInt(accessory3Row[0]));
-			$accessory3Gem1 = getGemById(parseInt(accessory3Row[1]));
-			$accessory3Gem2 = getGemById(parseInt(accessory3Row[2]));
-			$accessory3Gem3 = getGemById(parseInt(accessory3Row[3]));
-			$accessory3Enchant = getEnchantById(parseInt(accessory3Row[4]));
-			$accessory3Modifier = getModifierById(parseInt(accessory3Row[5]));
+				$accessory3 = getAccessoryById(parseInt(accessory3Row[0]));
+				$accessory3Gem1 = getGemById(parseInt(accessory3Row[1]));
+				$accessory3Gem2 = getGemById(parseInt(accessory3Row[2]));
+				$accessory3Gem3 = getGemById(parseInt(accessory3Row[3]));
+				$accessory3Enchant = getEnchantById(parseInt(accessory3Row[4]));
+				$accessory3Modifier = getModifierById(parseInt(accessory3Row[5]));
 
-			$chestplate1 = getChestplateById(parseInt(chestplate1Row[0]));
-			$chestplate1Gem1 = getGemById(parseInt(chestplate1Row[1]));
-			$chestplate1Gem2 = getGemById(parseInt(chestplate1Row[2]));
-			$chestplate1Gem3 = getGemById(parseInt(chestplate1Row[3]));
-			$chestplate1Enchant = getEnchantById(parseInt(chestplate1Row[4]));
-			$chestplate1Modifier = getModifierById(parseInt(chestplate1Row[5]));
+				$chestplate1 = getChestplateById(parseInt(chestplate1Row[0]));
+				$chestplate1Gem1 = getGemById(parseInt(chestplate1Row[1]));
+				$chestplate1Gem2 = getGemById(parseInt(chestplate1Row[2]));
+				$chestplate1Gem3 = getGemById(parseInt(chestplate1Row[3]));
+				$chestplate1Enchant = getEnchantById(parseInt(chestplate1Row[4]));
+				$chestplate1Modifier = getModifierById(parseInt(chestplate1Row[5]));
 
-			$pants1 = getPantsById(parseInt(pants1Row[0]));
-			$pants1Gem1 = getGemById(parseInt(pants1Row[1]));
-			$pants1Gem2 = getGemById(parseInt(pants1Row[2]));
-			$pants1Gem3 = getGemById(parseInt(pants1Row[3]));
-			$pants1Enchant = getEnchantById(parseInt(pants1Row[4]));
-			$pants1Modifier = getModifierById(parseInt(pants1Row[5]));
-
-			playCorrect();
+				$pants1 = getPantsById(parseInt(pants1Row[0]));
+				$pants1Gem1 = getGemById(parseInt(pants1Row[1]));
+				$pants1Gem2 = getGemById(parseInt(pants1Row[2]));
+				$pants1Gem3 = getGemById(parseInt(pants1Row[3]));
+				$pants1Enchant = getEnchantById(parseInt(pants1Row[4]));
+				$pants1Modifier = getModifierById(parseInt(pants1Row[5]));
+				playCorrect();
+			} else {
+				playWrong();
+			}
 		} catch (error) {
 			playWrong();
 		}
