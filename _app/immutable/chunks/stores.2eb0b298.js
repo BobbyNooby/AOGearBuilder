6c28778e0aed8b6e98fc7647c9e41eb754c7f48c
BobbyNooby <theengineering.commandgamer@gmail.com>
2024-02-26 06:34:45 +0000

    Updates

---
+++ _app/immutable/chunks/stores.2eb0b298.js
@@ -0,0 +1,1 @@
+import{d as e}from"./singletons.033c8950.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
