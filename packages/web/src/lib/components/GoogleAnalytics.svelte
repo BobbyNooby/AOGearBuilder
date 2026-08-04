a1d98088125e27fd6dab93d027c799ea2d3cee5d
BobbyNooby <theengineering.commandgamer@gmail.com>
2026-08-04 18:47:09 +0100

    feat(web): add Google Analytics (G-6FB1RCQJ7W) via layout component

---
+++ packages/web/src/lib/components/GoogleAnalytics.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-6FB1RCQJ7W"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){"{dataLayer.push(arguments)}"}
+		gtag('js', new Date());
+		gtag('config', 'G-6FB1RCQJ7W');
+	</script>
+</svelte:head>
--- packages/web/src/routes/+layout.svelte
+++ packages/web/src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script>
 	import '../app.css';
 	import AppShell from '$lib/components/Layout/AppShell.svelte';
+	import GoogleAnalytics from '$lib/components/GoogleAnalytics.svelte';
 	let { children, data } = $props();
 </script>
 
+<GoogleAnalytics />
 <AppShell user={data.user} permissions={data.permissions}>
 	{@render children()}
 </AppShell>
